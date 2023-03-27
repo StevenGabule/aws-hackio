1c780c6c8889eba4a1db92eb285ad61cb4f39d4d
John Paul <lucas.gabule@gmail.com>
2023-03-27 20:56:19 +0800

    setup the aws ses

--- backend/controllers/auth/register.controller.js
+++ backend/controllers/auth/register.controller.js
@@ -1,3 +1,42 @@
+const AWS = require('aws-sdk');
+
+// accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+// secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+// region: process.env.AWS_REGION
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
+})
+
+const ses = new AWS.SES({apiVersion: '2010-12-01'})
+
 exports.register = (req, res) => {
-  res.status(201).json({user: req.body})
+  try {
+    const {name, email, password} = req.body;
+    const params = {
+      Source: process.env.EMAIL_FROM,
+      Destination: {
+        ToAddresses: [email]
+      },
+      ReplyToAddresses: [process.env.EMAIL_TO],
+      Message: {
+        Body: {
+          Html: {
+            Charset: 'UTF-8',
+            Data: `<html><body><h1>Hello ${name}</h1><p>Test email</p></body></html>`
+          }
+        },
+        Subject: {
+          Charset: 'UTF-8',
+          Data: 'Complete your registration'
+        }
+      }
+    }
+    const sendingEmail = ses.sendEmail(params).promise();
+    sendingEmail.then(() => res.send('Email sent!')).catch(err => console.log(err.stack))
+  } catch (error) {
+    console.log(error.message);
+  }
 }
